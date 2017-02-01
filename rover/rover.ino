15eca2124328a85a9a0d4e783def63fc0238a8e2
mtalhaf93 <mtalhaf@gmail.com>
2017-02-01 18:14:51 +0000

    added initial comment and version.

--- rover/rover.ino
+++ rover/rover.ino
@@ -1,3 +1,12 @@
+/*
+ This code runs the rover INSERT WHAT ROVER DOES.
+
+ The code is supposed to run on the Arduino UNO and is built for
+ SOFT561 module as a project.
+
+ VERSION: 0.1.0
+ */
+ 
 void setup() {
   // put your setup code here, to run once:
 
