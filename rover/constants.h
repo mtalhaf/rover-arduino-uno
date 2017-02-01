290da0b01b3ddc6696f4aace08a8ecf7a6e4a4ca
mtalhaf93 <mtalhaf@gmail.com>
2017-02-01 18:44:06 +0000

    added contants files, included constants for motor A and B pins.

---
+++ rover/constants.h
@@ -0,0 +1,11 @@
+/*
+ * Constants file to include all the constants required by the main program
+ */
+
+#define motorA_dir_pin 12 //Direction pin number for motor A
+#define motorA_speed_pin 3 //Speed pin number for motor A, the pin is analog PWM 
+#define motorA_brake_pin 9 //Brake pin number for motor A
+
+#define motorB_dir_pin 13 //Direction pin number for motor B
+#define motorB_speed_pin 11 //Speed pin number for motor B, the pin is analog PWM
+#define motorB_brake_pin 8 //Brake pin number for motor B
