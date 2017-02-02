9a03b94fc21cd5f5c19e5fe6ff6f19aa85bbb8b1
mtalhaf93 <mtalhaf@gmail.com>
2017-02-02 12:34:26 +0000

    added method to avoid an obstacle by turning in a random direction.

--- rover/obstacle.h
+++ rover/obstacle.h
@@ -3,22 +3,37 @@
  */
 
 #include "ultrasonic.h"
+#include "movement.h"
 
 /*
  * detects obstacles in the rovers path 
  */
 
-boolean detectObstacles(long detectionDistance){
+boolean detectObstacles(){
 
   long distanceToObject; // distance to the detected object
   
   echoUltraSonic(); //echos the ultra sonic to check for obstacles
   distanceToObject = getDistance();
 
-  if (distanceToObject <= detectionDistance)
+  if (distanceToObject <= OBSTACLE_DETECTION_DISTANCE)
     return true;
   else
     return false;
     
+}
+
+void avoidObstacle(){
+
+  int turnDirection = random(1,3); // initialise random turn to take left/ right
+  int roverDirection = random(1,3); // initialise random direction to take forward/ back
+  boolean obstacle = true; // variable to see if obstacle is still in place
+  
+  // if obstacle is still there keep turning the rover
+  while(obstacle){
+    turnRover(255, roverDirection, turnDirection, 2000); // turns the rover at full speed in the random turn and direction for 2 seconds
+    obstacle = detectObstacles();
+  }
+  
 }
 
