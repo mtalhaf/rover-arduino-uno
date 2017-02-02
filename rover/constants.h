b5b3ecd79d0f0b697c36bdc1e632c6afef5f7a8a
mtalhaf93 <mtalhaf@gmail.com>
2017-02-02 12:21:07 +0000

    added constants for ultrasonic rangefinder.

--- rover/constants.h
+++ rover/constants.h
@@ -10,6 +10,9 @@
 #define motorB_speed_pin 11 //Speed pin number for motor B, the pin is analog PWM
 #define motorB_brake_pin 8 //Brake pin number for motor B
 
+#define ultrasonic_trigger_pin 4 //Trigger pin for the ultra sonic range finder in fron of the rover
+#define ultrasonic_echo_pin 7 //Echo pin for the ultra sonic range finder in fron of the rover
+
 #define ROVER_FORWARD_DIRECTION 1 //rover forward direction
 #define ROVER_BACKWARD_DIRECTION 2 //rover backward direction
 
