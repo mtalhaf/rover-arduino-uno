45019fec0538d2a688afbe31d3fa00e78eaa61db
mtalhaf93 <mtalhaf@gmail.com>
2017-02-08 20:21:27 +0000

    moved all initialisation code from rover.ino to init.h.

---
+++ rover/init.h
@@ -0,0 +1,86 @@
+/*
+ * Init file to include all the Initialisation required by the main program
+ */
+
+#ifndef init_h
+#define init_h
+
+
+#include "constants.h"
+#include "Motor.h"
+#include "Movement.h"
+#include "Ultrasonic.h"
+#include "ObstacleDetection.h"
+#include "lcd.h"
+
+
+Motor* motorA;
+Motor* motorB;
+Ultrasonic* ultrasonic;
+Movement* movement;
+ObstacleDetection* obstacleDetection;
+
+
+
+/*
+ * Sets up all the required sensor objects
+ */
+
+void setUpSensorObjects(){
+  motorA = new Motor(motorA_dir_pin, motorA_brake_pin, motorA_speed_pin);
+  motorB = new Motor(motorB_dir_pin, motorB_brake_pin, motorB_speed_pin);
+  ultrasonic = new Ultrasonic(ultrasonic_front_trigger_pin, ultrasonic_front_echo_pin);
+}
+
+
+/*
+ * Sets up all the required functional objects
+ */
+
+void setUpFunctionalObjects(){
+  movement = new Movement(motorA, motorB);
+  obstacleDetection = new ObstacleDetection(movement, ultrasonic);
+}
+
+/*
+ * Sets up motor pins in the main setup function
+ * goes through direction and brake pins of motor
+ * A and B and initiates them as output pins to 
+ * send signals to the motor.
+ * 
+ * The speed pin does not needs to be initiated as
+ * its a dedicated output PWM pin.
+ */
+
+void setUpMotorPins() {
+  motorA.setUp(); // sets up motor A pins
+  motorB.setUp(); // sets up motor B pins
+}
+
+
+/*
+ * Sets up the pins for the ultrasonic rangefinder.
+ * the trigger pin is used as an output and the echo
+ * pin is to read the input from the rangefinder.
+ */
+
+void setUpUltraSonicRangeFinderPins() {
+  ultrasonic.setUp();
+}
+
+/*
+ * Sets up LCD for printing
+ */
+
+void setUpLCD() {
+  
+  lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
+  lcd.setCursor(0,0); //Start at character 0 on line 0
+}
+
+void init(){
+  setUpMotorPins(); //sets up all the motor pins
+  setUpUltraSonicRangeFinderPins(); //sets up all ultra sonic pins
+  setUpLCD(); //sets up the LCD
+}
+#endif
--- rover/rover.ino
+++ rover/rover.ino
@@ -7,75 +7,12 @@
  VERSION: v0.1.3
  */
 
+#include "init.h"
 #include "obstacleDetection.h"
-#include "motor.h"
 #include "movement.h"
-#include "ultrasonic.h"
-
-/*
- * Sets up motor pins in the main setup function
- * goes through direction and brake pins of motor
- * A and B and initiates them as output pins to 
- * send signals to the motor.
- * 
- * The speed pin does not needs to be initiated as
- * its a dedicated output PWM pin.
- */
-
-Motor motorA(motorA_dir_pin, motorA_brake_pin, motorA_speed_pin);
-Motor motorB(motorB_dir_pin, motorB_brake_pin, motorB_speed_pin);
-//Ultrasonic ultrasonic(ultrasonic_trigger_pin, ultrasonic_echo_pin);
-
-//Movement movement;
-
-void setUpMotorPins() {
-  
-  //pinMode(motorA_dir_pin, OUTPUT); //set motor A direction pin to output
-  //pinMode(motorA_brake_pin, OUTPUT); //set motor A brake pin to output
-
-  //pinMode(motorB_dir_pin, OUTPUT); //set motor B direction pin to output
-  //pinMode(motorB_brake_pin, OUTPUT); //set motor B brake pin to output
-
-  motorA.setUp(); // sets up motor A pins
-  motorB.setUp(); // sets up motor B pins
-}
-
-/*
- * Sets up the pins for the ultrasonic rangefinder.
- * the trigger pin is used as an output and the echo
- * pin is to read the input from the rangefinder.
- */
-
-void setUpUltraSonicRangeFinderPins() {
-  //pinMode(ultrasonic_trigger_pin, OUTPUT); //set ultrasonic trigger pin to output
-  // pinMode(ultrasonic_echo_pin, INPUT); //set ultrasonic echo pin to output
-
-  ultrasonic.setUp();
-  
-}
-
-/*
- * Sets up LCD for printing, prints out welcome message
- */
-
-void setUpLCD() {
-  
-  lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
-
-  lcd.setCursor(0,0); //Start at character 0 on line 0
-  lcd.print("Hello!");
-  lcd.setCursor(0,1);
-  lcd.print("I am your rover");
-  lcd.setCursor(0,0); //Start at character 0 on line 0
-  
-}
 
 void setup() {
-
-  setUpMotorPins(); //sets up all the motor pins
-  setUpUltraSonicRangeFinderPins(); //sets up all ultra sonic pins
-  setUpLCD(); //sets up the LCD
-  
+  init(); // from init.h
 }
 
 /*
@@ -85,8 +22,8 @@ void moveRoverAround(){
 
   movement.moveForward(ROVER_SPEED);
   
-  if (detectObstacles()) //if obstacles are detected in front of the rover
-    avoidObstacle(); //avoid the obstacles
+  if (obstacleDetection.detectObstacles()) //if obstacles are detected in front of the rover
+    obstacleDetection.avoidObstacle(); //avoid the obstacles
 }
 
 /*
