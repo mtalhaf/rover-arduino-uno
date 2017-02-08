fb4631b9b42a6d77aae60253629c2a17c69feb0e
mtalhaf93 <mtalhaf@gmail.com>
2017-02-08 20:20:37 +0000

    updated lcd.h to include #ifndef.

--- rover/lcd.h
+++ rover/lcd.h
@@ -1,9 +1,15 @@
 /*
- * This code intialises the lcd screen
+ * This file is used to initialise LCD for printing
  */
+
+#ifndef lcd_h
+#define lcd_h
 
 #include "Wire.h"
 #include "LiquidCrystal_I2C.h" // Library included seperately to control the LCD display
 
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+
+#endif
+
 
