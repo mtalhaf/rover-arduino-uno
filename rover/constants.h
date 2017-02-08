1520d30da58c83ce99f4dc4d4ab7e982c3d888e5
mtalhaf93 <mtalhaf@gmail.com>
2017-02-08 14:00:09 +0000

    added ifdef check in constants.

--- rover/constants.h
+++ rover/constants.h
@@ -2,6 +2,9 @@
  * Constants file to include all the constants required by the main program
  */
 
+#ifndef constants_h
+#define constants_h
+
 #define motorA_dir_pin 12 //Direction pin number for motor A
 #define motorA_speed_pin 3 //Speed pin number for motor A, the pin is analog PWM 
 #define motorA_brake_pin 9 //Brake pin number for motor A
@@ -10,8 +13,8 @@
 #define motorB_speed_pin 11 //Speed pin number for motor B, the pin is analog PWM
 #define motorB_brake_pin 8 //Brake pin number for motor B
 
-#define ultrasonic_trigger_pin 4 //Trigger pin for the ultra sonic range finder in fron of the rover
-#define ultrasonic_echo_pin 7 //Echo pin for the ultra sonic range finder in fron of the rover
+#define ultrasonic_front_trigger_pin 4 //Trigger pin for the ultra sonic range finder in fron of the rover
+#define ultrasonic_back_echo_pin 7 //Echo pin for the ultra sonic range finder in fron of the rover
 
 #define ROVER_FORWARD_DIRECTION 1 //rover forward direction
 #define ROVER_BACKWARD_DIRECTION 2 //rover backward direction
@@ -22,3 +25,5 @@
 #define ROVER_SPEED 255 //rover speed settings
 
 #define OBSTACLE_DETECTION_DISTANCE 25 //maximum distance between rover and an obstacle in cm
+
+#endif
