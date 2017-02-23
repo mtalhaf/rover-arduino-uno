6c457ff614ee57a95b0069422e1ab63d17bb21be
mtalhaf93 <mtalhaf@gmail.com>
2017-02-23 01:19:09 +0000

    added publisher and subscriber for test purposes.

--- rover/rosInit.h
+++ rover/rosInit.h
@@ -8,15 +8,41 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 #include "rosConstants.h"
-#include "rosCallBacks.h"
 #include "LiquidCrystal_I2C.h"
 
 ros::NodeHandle rosNodeHandle;
+LiquidCrystal_I2C* lcd = new LiquidCrystal_I2C(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+std_msgs::String str_msg;
+char hello[13] = "hello world!";
+
+
+ros::Publisher chatter("chatter", &str_msg);
+
+
+void displayOnLcd(const std_msgs::String message){
+  lcd->clear();
+  lcd->setCursor(0,0);
+  lcd->print(message.data);   // display on lcd
+
+  str_msg.data = hello;
+  chatter.publish( &str_msg );
+
+  str_msg.data = message.data;
+  chatter.publish( &str_msg );
+}
+
+
+
+
+ros::Subscriber<std_msgs::String> lcdSubscriber("topicDisplayOnLcd", &displayOnLcd );
+
 
 void rosSetup()
 { 
   rosNodeHandle.initNode();
   rosNodeHandle.subscribe(lcdSubscriber);
+  rosNodeHandle.advertise(chatter);
 }
 
 void rosLoop()
