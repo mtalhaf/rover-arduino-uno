7be4971d02038dcc3859da894e829455fa562a8d
mtalhaf93 <mtalhaf@gmail.com>
2017-02-26 14:20:33 +0000

    added all lcd and movement subscribers.

--- rover/rosInit.h
+++ rover/rosInit.h
@@ -6,18 +6,31 @@
 #define rosInit_h
 
 #include "rosLcdCallback.h"
+#include "rosMovementCallback.h"
 
 
+//get the node handler instance
 ros::NodeHandle rosNodeHandle;
 
 void rosSetup()
 { 
+  //initialises node
   rosNodeHandle.initNode();
+  
+  //subscribes to the topic being published
   rosNodeHandle.subscribe(lcdDisplaySubscriber);
+  rosNodeHandle.subscribe(lcdClearDisplaySubscriber);
+
+  rosNodeHandle.subscribe(movementMoveForwardSubscriber);
+  rosNodeHandle.subscribe(movementMoveBackwardSubscriber);
+  rosNodeHandle.subscribe(movementTurnRoverLeftSubscriber);
+  rosNodeHandle.subscribe(movementTurnRoverRightSubscriber);
+  rosNodeHandle.subscribe(movementStopRoverSubscriber);
 }
 
 void rosLoop()
-{  
+{ 
+  //spin once handles all the callbacks and ros related features 
   rosNodeHandle.spinOnce();
   delay(1);
 }
