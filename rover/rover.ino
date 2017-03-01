c767e8a0b29a68434d637cd6d66b8d6c22fa3938
mtalhaf93 <mtalhaf@gmail.com>
2017-03-01 14:03:01 +0000

    removed code printing things on the lcd.

--- rover/rover.ino
+++ rover/rover.ino
@@ -69,37 +69,22 @@ void moveInAllDirections(){
 void rosCommands(){
   if (stopRoverMovement){
     movement->stopRoverMotors();
-    lcd->clear();
-    lcd->setCursor(0,0);
-    lcd->print(F("stopping"));
   }
 
   if (moveForward){
     movement->moveForward(ROVER_SPEED);
-    lcd->clear();
-    lcd->setCursor(0,0);
-    lcd->print(F("forward"));
   }
 
   if (moveBackward){
      movement->moveBackward(ROVER_SPEED);
-     lcd->clear();
-     lcd->setCursor(0,0);
-     lcd->print(F("backward"));
   }
 
   if (turnLeft){
     movement->turnRoverWithoutMovement(ROVER_SPEED, ROVER_TURN_LEFT);
-    lcd->clear();
-    lcd->setCursor(0,0);
-    lcd->print(F("left"));
   }
 
   if (turnRight){
     movement->turnRoverWithoutMovement(ROVER_SPEED, ROVER_TURN_RIGHT);
-    lcd->clear();
-    lcd->setCursor(0,0);
-    lcd->print(F("right"));
   }
 }
 
